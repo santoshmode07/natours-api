c6be96eaead83076ed4ffe0ec6a1f9734c2ac854
santoshmode07 <santoshmode7@gmail.com>
2025-12-31 15:14:53 +0530

    created a model [instance of schema ]and added a data to it

--- server.js
+++ server.js
@@ -20,12 +20,41 @@ mongoose
     console.log('DB connection successful!');
   });
 
-  
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'The Hamster Hiker',
+  price: 837,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR 💥:', err);
+  });
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
-
 process.on('SIGTERM', () => {
   console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
   server.close(() => {
