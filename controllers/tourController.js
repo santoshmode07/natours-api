c16b7b55efecd68b388cded224341a1fe277b616
santoshmode07 <santoshmode7@gmail.com>
2026-01-03 11:02:59 +0530

    Making the API better with adding of Filtering[operations]

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,24 +3,30 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     //BUILD QUERY
+    //1)FILTERING
     const queryObj = { ...req.query }; //shallow copy
 
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
 
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    console.log(req.query, queryObj);
+    //2)ADVANCED FILTERING
+    console.log(queryObj);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = await Tour.find(queryObj);
+    console.log(JSON.parse(queryStr));
+
+    const query = await Tour.find(JSON.parse(queryStr));
+
+    //EXECUTE QUERY
+    const tours = await query;
 
     // const query= await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    //EXECUTE QUERY
-    const tours = await query;
 
     res.status(200).json({
       status: 'success',
