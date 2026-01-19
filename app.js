774094b06722e9384239772de8e2583840188ccd
santoshmode07 <santoshmode7@gmail.com>
2026-01-19 19:10:22 +0530

    Handling unhandled routes

--- app.js
+++ app.js
@@ -17,11 +17,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -51,6 +46,14 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
 // 4)START SERVER
 
 module.exports = app;
