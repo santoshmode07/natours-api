54fd0f7d7df02f766b3ff238ec0597cd58f632e1
santoshmode07 <santoshmode7@gmail.com>
2026-02-09 22:46:06 +0530

    Updating user data with the API

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,15 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
-
+import { updateData } from './updateSettings';
 // DOM ELEMENTS
 const loginForm = document.querySelector('.form--login');
 
 const mapBox = document.getElementById('map');
 
 const logOutBtn = document.querySelector('.nav__el--logout');
+
+const userDataForm = document.querySelector('.form-user-data');
 
 // DELEGATION
 
@@ -25,3 +27,10 @@ if (loginForm)
   });
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+if (userDataForm)
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+    updateData(name, email);
+  });
